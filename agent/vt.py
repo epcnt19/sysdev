10452e168bbaf235d80cb6471227ba53eeff1a82
epcnt19 <epcnt19@gmail.com>
2018-10-21 00:05:06 +0900

    create vt.pt

---
+++ agent/vt.py
@@ -0,0 +1,26 @@
+#coding:utf-8
+
+import virus_total_apis
+import json
+
+
+class Virustotal:
+	
+	def __init__(self,apikey):
+		self.api = virus_total_apis.PublicApi(apikey)
+
+	def scan(self,filepath):
+		response_scan = self.api.scan_file(filepath)
+		filehash = response_scan["results"]["sha1"]
+		return filehash
+
+	def report(self,filehash):
+		result = ""
+		response_report = self.api.get_file_report(filehash)
+		json_dict = json.loads(json.dumps(response_report))
+
+		for key,value in json_dict["results"].items():
+			if key != "scans":
+				result += "{0} : {1}\n".format(key,value)
+		
+		return result
